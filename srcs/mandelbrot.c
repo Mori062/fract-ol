df0ae6f9f50c93ec0abc28344187d9eac5276677
Mori062 <01morishi@gmail.com>
2023-07-21 12:22:22 +0900

    add

--- srcs/mandelbrot.c
+++ srcs/mandelbrot.c
@@ -6,28 +6,41 @@
 /*   By: morishitashoto <morishitashoto@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 12:37:05 by morishitash       #+#    #+#             */
-/*   Updated: 2023/07/14 20:22:34 by morishitash      ###   ########.fr       */
+/*   Updated: 2023/07/14 21:55:44 by morishitash      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/fractol.h"
 #include <stdio.h>
 
-typedef struct s_mlx
+typedef struct s_data
 {
-	void	*mlx;
-	void	*win;
-}	t_mlx;
+	double    cx;//中心座標
+	double    cy;//中心座標
 
-typedef struct s_mandelbrot
+	double    scale;//倍率
+
+	double    c_re;//コマンドライン実部
+	double    c_im;//コマンドライン虚部
+
+	int        flag;//mandelbrot:0 julia:1
+
+	int	tmp;
+
+	void    *mlx;
+	void    *win;
+	void    *img;
+	char    *addr;
+	int      bits_per_pixel;
+	int        line_length;
+	int        endian;
+}            t_data;
+
+typedef struct s_complex
 {
-	int		x;
-	int		y;
-	float	center_x;
-	float	center_y;
-	float	zx;
-	float	zy;
-}	t_mandelbrot;
+	double    re;
+	double    im;
+}            t_complex;
 
 void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 {
@@ -37,58 +50,46 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
-void	mandel_init(t_mandelbrot *mand)
-{
-	mand->zx = 0;
-	mand->zy = 0;
-	mand->center_x = (mand->x - DPWID / 2) * 4.0 / DPWID;
-	mand->center_y = (mand->y - DPHEI / 2) * 4.0 / DPWID;
-}
-
-void	mandel_calc(t_mandelbrot *mand)
-{
-	float	tmp;
-
-	tmp = mand->zx * mand->zx - mand->zy * mand->zy + mand->center_x;
-	mand->zy = 2 * mand->zx * mand->zy + mand->center_y;
-	mand->zx = tmp;
-}
-
 int	mandelbrot(void)
 {
-	t_mlx			mlx;
 	t_data			img;
-	t_mandelbrot	mand;
 	float			i;
+	int				x;
+	int				y;
 
-	mlx.mlx = mlx_init();
-	mlx.win = mlx_new_window(mlx.mlx, DPWID, DPHEI, "fractol");
-	img.img = mlx_new_image(mlx.mlx, DPWID, DPHEI);
+	img.mlx = mlx_init();
+	img.win = mlx_new_window(img.mlx, DPWID, DPHEI, "fractol");
+	img.img = mlx_new_image(img.mlx, DPWID, DPHEI);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel,
 			&img.line_length, &img.endian);
-	mand.x = 0;
-	while (mand.x < DPWID)
+	x = 0;
+	while (x < DPWID)
 	{
-		mand.y = 0;
-		while (mand.y < DPHEI)
+		y = 0;
+		while (y < DPHEI)
 		{
-			mandel_init(&mand);
+			img.cx = (x - DPWID / 2) * 4.0 / DPWID;
+			img.cy = (y - DPHEI / 2) * 4.0 / DPWID;
+			img.c_re = 0;
+			img.c_im = 0;
 			i = 0;
-			while (mand.zx * mand.zx + mand.zy * mand.zy <= 4 && i < 50)
+			while (img.c_re * img.c_re + img.c_im * img.c_im < 4 && i < 50)
 			{
-				mandel_calc(&mand);
+				img.tmp = img.c_re * img.c_re - img.c_im * img.c_im + img.cx;
+				img.c_im = 2 * img.c_re * img.c_im + img.cy;
+				img.c_re = img.tmp;
 				i++;
 			}
 			if (i == 50)
-				my_mlx_pixel_put(&img, mand.x, mand.y, 0x000000);
+				my_mlx_pixel_put(&img, x, y, 0x000000);
 			else
-				my_mlx_pixel_put(&img, mand.x, mand.y, 0xFFFFFF * i / 5);
-			mand.y++;
+				my_mlx_pixel_put(&img, x, y, 0xFFFFFF * i / 5);
+			y++;
 		}
-		mand.x++;
+		x++;
 	}
-	mlx_put_image_to_window(mlx.mlx, mlx.win, img.img, 0, 0);
-	mlx_loop(mlx.mlx);
+	mlx_put_image_to_window(img.mlx, img.win, img.img, 0, 0);
+	mlx_loop(img.mlx);
 	return (0);
 }
 
